f3fc8e6e47cfb01164549c2e0f47ba8b6c6aca09
Cloud User <ec2-user@ip-172-31-22-27.ap-southeast-1.compute.internal>
2023-06-01 15:37:59 +0000

    all new files

---
+++ pallindrome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Even number script
+NUM=$1
+if [ `expr SUM % 2` -eq 0]
+then
+  echo "${NUM is Even}"
+else
+  echo "${NUM is odd}"
+ fi #!/bin/bash
+
+This is a program which is going 
